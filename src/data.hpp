59c78b426e5a833b5657adde4c78a52318979a87
ilim <ilim0t14@gmail.com>
2019-06-19 01:17:21 +0900

    add norm 1&inf

--- src/data.hpp
+++ src/data.hpp
@@ -38,4 +38,26 @@ public:
             }
         }
     };
+
+    double norm_1() const {
+        std::array<double, M> buf;
+        buf.fill(0);
+        for (const auto raw : *this) {
+            for (int j = 0; j < this->col_size; ++j) {
+                buf.at(j) += std::abs(raw.at(j));
+            }
+        }
+        return *std::max_element(buf.begin(), buf.end());
+    }
+
+    double norm_inf() const {
+        std::array<double, N> buf;
+        buf.fill(0);
+        for (int i = 0; i < this->raw_size; ++i) {
+            for (const auto x : this->at(i)) {
+                buf.at(i) += std::abs(x);
+            }
+        }
+        return *std::max_element(buf.begin(), buf.end());
+    }
 };
---
+++ test/norm.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include <complex>
+#include "data.hpp"
+
+TEST(Norm, One) {
+    EXPECT_EQ((Mat<double, 2, 3>{
+            {0, 1, 2},
+            {4, 8, 16}}.norm_1()), 18);
+
+    EXPECT_EQ((Mat<double, 2, 3>{
+            {0, 1, 2},
+            {4, 8, 16}}.norm_inf()), 28);
+
+    EXPECT_EQ((Mat<std::complex<double>, 2, 3>{
+            {std::complex<double>(0, 0), std::complex<double>(4, 3),  std::complex<double>(12, 5)},
+            {std::complex<double>(3, 4), std::complex<double>(5, 12), std::complex<double>(8, 15)}
+    }.norm_1()), 30);
+
+    EXPECT_EQ((Mat<std::complex<double>, 2, 3>{
+            {std::complex<double>(0, 0), std::complex<double>(4, 3),  std::complex<double>(12, 5)},
+            {std::complex<double>(3, 4), std::complex<double>(5, 12), std::complex<double>(8, 15)}
+    }.norm_inf()), 35);
+}
