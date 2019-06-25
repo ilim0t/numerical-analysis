05a638b0a8efa7d04b303c8746320b72ce4eab89
oO0adeliae0Oo <lagrange4423@gmail.com>
2019-06-25 16:45:22 +0900

    newton法

--- src/iterative_method.hpp
+++ src/iterative_method.hpp
@@ -18,7 +18,7 @@ secant(const std::function<T(T)> &func, const std::function<bool(T)> &convergenc
                 func(xs.back()) * (xs.back() - xs.crbegin()[1]) / (func(xs.back()) - func(xs.crbegin()[1])));
         if (convergence_codition(xs.back())) {
             return {xs.back(), interative_num};
-            
+
 // logのやつは将来設計
 // first_value 初期値
 // second_value 初期値の次のx
@@ -31,8 +31,20 @@ secant(const std::function<T(T)> &func, const std::function<bool(T)> &convergenc
     }
     return {xs.back(), interative_num};
 }
+template<typename T>
 std::tuple<T, std::size_t>
-newton(const std::function<T(T)> &func, const std::function<bool(T)> &convergence_coditon, T first_value = 0
-        std::function<dfunc<T(T)>)
+newton(const std::function<T(T)> &func, const std::function<T(T)> &dfunc,  const std::function<bool(T)> &convergence_codition, T first_value = 0, std::size_t max_num = 1e+10) {
+    std::vector<T> xs = {first_value, dfunc};
+    int interative_num = 2;
 
-#endif //NUMERICAL_ANALYSIS_ITERATIVE_METHOD_HPP
+    for (; interative_num < max_num; ++interative_num) {
+        xs.push_back(
+                xs.back() - (func(xs.back())) / (dfunc(xs.back()))
+        );
+        if (convergence_codition(xs.back())) {
+            return {xs.back(), interative_num};
+        }
+    }
+}
+
+#endif
