0deb47dd8e37b0bd94d0c7d0dfa5b854b114ce22
ilim <ilim0t14@gmail.com>
2019-06-27 00:38:23 +0900

    add backward_substitution & pre-inverse

--- test/inverse_matrix.cpp
+++ test/inverse_matrix.cpp
@@ -8,3 +8,19 @@ TEST(Mat_Inverse, Forward_Substitution) {
                                     Mat<double, 1, 2>{{1, 0}})),
               (Mat<double, 1, 2>{{1, -3}}));
 }
+
+TEST(Mat_Inverse, Backward_Substitution) {
+    ASSERT_EQ((backward_substitution(Mat<double, 2, 2>{{1, 2},
+                                                       {0, -2}},
+                                     Mat<double, 1, 2>{{1, -3}})),
+              (Mat<double, 1, 2>{{-2, 3. / 2}}));
+}
+
+//TEST(Mat_Inverse, Mat_Inverse) {
+//    ASSERT_EQ((inverse(Mat<double, 2, 2>{{1, 2},
+//                                         {0, -2}},
+//                       Mat<double, 2, 2>{{1, 2},
+//                                         {0, -2}})),
+//              (Mat<double, 1, 2>{{-2,     1},
+//                                 {3. / 2, -1. / 2}}));
+//}
