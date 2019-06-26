8f996bab8371c40ca25a4d78a2c72a538de43cc1
ilim <ilim0t14@gmail.com>
2019-06-27 03:19:56 +0900

    add jacobi method

--- test/iterative_method_test.cpp
+++ test/iterative_method_test.cpp
@@ -27,3 +27,22 @@ TEST(Iterative_Method, Secant_Method) {
             std::function<double(double)>([](double x) { return x * x - 4; }),
             error(1e-5))), 2, 1e-3);
 }
+
+TEST(Iterative_Method, Jacobi_Method) {
+    ASSERT_NEAR((std::get<0>(
+            jacobi_method(Mat<double, 2, 2>{{3, 1},
+                                            {1, 3}},
+                          Mat<double, 1, 2>{{-2, 2}},
+                          std::function<bool(Mat<double, 1, 2>, Mat<double, 1, 2>)>(
+                                  [](Mat<double, 1, 2> x, Mat<double, 1, 2> x_pre) {
+                                      return (x - x_pre).norm_2() / x.norm_2() < 1e-3;
+                                  }), Mat<double, 1, 2>(0))
+    ).at(0, 0)), -1, 1e-3);
+
+    ASSERT_NEAR((std::get<0>(
+            jacobi_method(Mat<double, 2, 2>{{3, 1},
+                                            {1, 3}},
+                          Mat<double, 1, 2>{{-2, 2}},
+                          relative_error<double, 2>(1e-5), Mat<double, 1, 2>(0))
+    ).at(0, 1)), 1, 1e-3);
+}
