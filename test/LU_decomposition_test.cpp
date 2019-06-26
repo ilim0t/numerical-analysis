7572408d22bea443045f758491a6b029999563c0
ilim <ilim0t14@gmail.com>
2019-06-27 01:02:33 +0900

    add test

--- test/LU_decomposition_test.cpp
+++ test/LU_decomposition_test.cpp
@@ -3,5 +3,16 @@
 #include "LU_decomposition.hpp"
 
 
-//TEST(LU_decomposition) {
-//}
+TEST(LU_decomposition, L) {
+    EXPECT_EQ((LU_decomposition(Mat<double, 2, 2>{{1, 2},
+                                                  {3, 4}}).first),
+              (Mat<double, 2, 2>{{1, 0},
+                                 {3, 1}}));
+}
+
+TEST(LU_decomposition, U) {
+    EXPECT_EQ((LU_decomposition(Mat<double, 2, 2>{{1, 2},
+                                                  {3, 4}}).first),
+              (Mat<double, 2, 2>{{1, 2},
+                                 {0, -2}}));
+}
