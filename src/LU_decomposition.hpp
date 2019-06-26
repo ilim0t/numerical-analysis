9ccf88a2bd7fa9411b05d743a208b751fd92240f
ilim <ilim0t14@gmail.com>
2019-06-27 00:47:12 +0900

    reformat

--- src/LU_decomposition.hpp
+++ src/LU_decomposition.hpp
@@ -10,26 +10,27 @@
 #include <data.hpp>
 #include <gaussian_elimination.hpp>
 
-template <typename T, std::size_t N>
-std::pair<Mat<T, N, N>, Mat<T, N, N> > LU_decomposition(const Mat<T, N, N> &mat){
+template<typename T, std::size_t N>
+std::pair<Mat<T, N, N>, Mat<T, N, N> > LU_decomposition(const Mat<T, N, N> &mat) {
     Mat<T, N, N> L(0);
     Mat<T, N, N> U(0);
-    for(int i = 0;i < N;i++){
-        for(int j = 0;j < N;j++){
-            if(i >= j){
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (i >= j) {
                 U.at(i, j) = mat.at(i, j);
-            }else{
+            } else {
                 L.at(i, j) = mat.at(i, j);
             }
-            if(i == j){
+            if (i == j) {
                 L.at(i, j) = 1;
             }
         }
     }
     return {L, U};
 }
-template <typename T, std::size_t N>
-Mat<T, 1, N> LU_Gaussian(const Mat<T, N, N> &mat, const Mat<T, 1, N> &vec){
+
+template<typename T, std::size_t N>
+Mat<T, 1, N> LU_Gaussian(const Mat<T, N, N> &mat, const Mat<T, 1, N> &vec) {
     auto LU = LU_decomposition(mat);
     auto y = gaussian_elimination(LU.first, vec);
     auto x = gaussian_elimination(LU.second, vec);
---
+++ test/LU_decomposition_test.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+#include "data.hpp"
+#include "LU_decomposition.hpp"
+
+
+//TEST(LU_decomposition) {
+//}
