4a0ca700a85083fc462e518c9c3d0d8a82d19176
ilim <ilim0t14@gmail.com>
2019-06-27 02:04:11 +0900

    fix add & improve LU_decomposition

--- src/LU_decomposition.hpp
+++ src/LU_decomposition.hpp
@@ -1,40 +1,56 @@
-//
-// Created by oO0adeliae0Oo on 2019-06-26.
-//
 #ifndef NUMERICAL_ANALYSIS_LU_DECOMPOSITION_HPP
 #define NUMERICAL_ANALYSIS_LU_DECOMPOSITION_HPP
 
-#include <iostream>
-#include <utility>
-#include <cmath>
 #include <data.hpp>
 #include <gaussian_elimination.hpp>
+#include <utility>
 
 template<typename T, std::size_t N>
 std::pair<Mat<T, N, N>, Mat<T, N, N> > LU_decomposition(const Mat<T, N, N> &mat) {
     Mat<T, N, N> L(0);
-    Mat<T, N, N> U(0);
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            if (i >= j) {
-                U.at(i, j) = mat.at(i, j);
-            } else {
-                L.at(i, j) = mat.at(i, j);
+    auto U = mat.copy();
+
+    for (std::size_t i = 0; i < N; ++i) {
+        L.at(i, i) = 1;
+        for (std::size_t j = i + 1; j < N; ++j) {
+            L.at(j, i) = U.at(j, i) / U.at(i, i);
+            for (std::size_t k = 0; k < N - i; ++k) {
+                U.at(j, N - k - 1) -= U.at(i, N - k - 1) * U.at(j, i) / U.at(i, i);
             }
-            if (i == j) {
-                L.at(i, j) = 1;
-            }
+        }
+    }
+
+    //    Lが下三角行列かどうかの確認
+    for (std::size_t i = 0; i < N; ++i) {
+        assert(L.at(i, i) == 1);
+        for (std::size_t j = i + 1; j < N; ++j) {
+            assert(L.at(i, j) == 0);
+        }
+    }
+    //    Uが上三角行列かどうかの確認
+    for (std::size_t i = 0; i < N; ++i) {
+        assert(U.at(i, i) != 0);
+        for (std::size_t j = i + 1; j < N; ++j) {
+            assert(U.at(j, i) == 0);
         }
     }
     return {L, U};
 }
 
 template<typename T, std::size_t N>
-Mat<T, 1, N> LU_Gaussian(const Mat<T, N, N> &mat, const Mat<T, 1, N> &vec) {
-    auto LU = LU_decomposition(mat);
-    auto y = gaussian_elimination(LU.first, vec);
-    auto x = gaussian_elimination(LU.second, vec);
-    return x;
+Mat<T, 1, N> LU_gaussian_elimination(const Mat<T, N, N> &mat, const Mat<T, 1, N> &vec) {
+    const auto LU = LU_decomposition(mat);
+    const auto L = LU.first;
+    const auto U = LU.second;
+
+    const auto y = forward_erase(L, vec).second;
+    const auto back_substitutioned = back_substitution(U, y);
+
+    Mat<T, 1, N> result(0);
+    for (std::size_t i = 0; i < N; ++i) {
+        result.at(0, i) = back_substitutioned.second.at(0, i) / back_substitutioned.first.at(i, i);
+    }
+    return result;
 }
 
 #endif //NUMERICAL_ANALYSIS_LU_DECOMPOSITION_HPP
--- test/LU_decomposition_test.cpp
+++ test/LU_decomposition_test.cpp
@@ -12,7 +12,15 @@ TEST(LU_decomposition, L) {
 
 TEST(LU_decomposition, U) {
     EXPECT_EQ((LU_decomposition(Mat<double, 2, 2>{{1, 2},
-                                                  {3, 4}}).first),
+                                                  {3, 4}}).second),
               (Mat<double, 2, 2>{{1, 2},
                                  {0, -2}}));
+}
+
+
+TEST(LU_decomposition, LU_Gaussian_Elimination) {
+    EXPECT_EQ((LU_gaussian_elimination(Mat<double, 2, 2>{{1, 2},
+                                                         {3, 4}},
+                                       Mat<double, 1, 2>{{5, 11}})),
+              (Mat<double, 1, 2>{{1, 2}}));
 }
